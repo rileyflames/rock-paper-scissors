9196cdc4bd7ac45ecc2f7e01d28ab06790d46500
rileyflames <Rileyflamez@proton.me>
2024-06-18 17:12:22 +0200

    adjusted the mobile version

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-const choices =['rock', 'paper', 'scissors'];
+const choices =['rock', 'paper', 'scissors']; // make a variable to store the computer's choices
 
 
-const displayWinner = document.getElementById("message");
+const displayWinner = document.getElementById("message"); // make a container to store the the value to use in the future
 const robotMessage = document.getElementById("robot");
 const winsDisplay =document.getElementById('wins');
 const losesDisplay =document.getElementById('loses');
 
-let playerScore = 0;
+let playerScore = 0; // variable to store the player score and computer score
 let computerScore = 0;
 
-function playgame(playerchoice) {
+function playgame(playerchoice) { // function to randomly generate and compare the user input and with the computer choice 
 
-    const computerChoice =choices[Math.floor(Math.random() * 3)];
+    const computerChoice =choices[Math.floor(Math.random() * 3)]; // cast a variable to store the randomly generated numbers and assign them to a variable.
     
     let result;
 
@@ -30,7 +30,7 @@ function playgame(playerchoice) {
 
     // console.log(`${result} the computer chose ${computerChoice}`);
    displayWinner.textContent = result;
-   robotMessage.textContent = computerChoice;
+   robotMessage.textContent = computerChoice; 
 
 
    switch(result){
